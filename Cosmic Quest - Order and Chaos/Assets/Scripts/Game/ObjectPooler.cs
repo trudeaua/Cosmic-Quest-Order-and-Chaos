703d48cc2fa1a01b1159775dbdaaac57f7e3a8c1
Cam Van Ravens <camvanravens@outlook.com>
2019-11-13 20:05:59 -0500

    Projectiles working, implementing multi-object pooling system

--- Cosmic Quest - Order and Chaos/Assets/Scripts/Game/ObjectPooler.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Game/ObjectPooler.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ObjectPoolItem
+{
+    public int pooledAmount;
+    public GameObject pooledObject;
+    public bool expandable;
+}
+
 public class ObjectPooler : MonoBehaviour
 {
     #region Singleton
@@ -12,14 +20,46 @@ public class ObjectPooler : MonoBehaviour
         Instance = this;
     }
     #endregion
+
+    public List<ObjectPoolItem> pooledItems;
+    public List<GameObject> pooledObjects;
     
     private void Start()
     {
-        
+        pooledObjects = new List<GameObject>();
+
+        foreach (ObjectPoolItem item in pooledItems)
+        {
+            for (int i = 0; i < item.pooledAmount; i++)
+            {
+                GameObject obj = Instantiate(item.pooledObject);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
     }
 
-    private void Update()
+    public GameObject GetPooledObject(GameObject prefab)
     {
+        foreach (var obj in pooledObjects)
+        {
+            if (!obj.activeInHierarchy && obj == prefab)
+            {
+                return obj;
+            }
+        }
+
+        foreach (ObjectPoolItem item in pooledItems)
+        {
+            if (item.pooledObject == prefab && item.expandable)
+            {
+                GameObject obj = Instantiate(item.pooledObject);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+                return obj;
+            }
+        }
         
+        return null;
     }
 }
