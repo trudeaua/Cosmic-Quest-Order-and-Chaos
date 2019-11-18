1ba7fc0ebd3436ad011a62c3c2576f12373ba1d1
nessa-art <nessenceart@gmail.com>
2019-11-18 14:01:36 -0500

    removing Update function in Lever.cs

--- Cosmic Quest - Order and Chaos/Assets/Scripts/World/Lever.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/World/Lever.cs
@@ -18,24 +18,22 @@ public class Lever : Interactable
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            foreach (GameObject player in Players)
-            {
-                Interact(player.transform);
-            }
-        }
+
     }
     
     public override void Interact(Transform target)
     {
-        if (CanInteract(target))
+        foreach (GameObject player in Players)
         {
-            Debug.Log("Interacted with " + target.name);
-            Anim.enabled = true;
-            Anim.Play("LeverAnimation");
-            Anim.SetBool("LeverActivated", true);
+            if (CanInteract(target))
+            {
+                Debug.Log("Interacted with " + target.name);
+                Anim.enabled = true;
+                Anim.Play("LeverAnimation");
+                Anim.SetBool("LeverActivated", true);
+            }
         }
+       
     }
 
     void PauseAnimationEvent ()
---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/World/Platform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class for managing rock placement detection
+public class Platform : Interactable
+{
+    protected Animator Anim;
+    protected GameObject[] Rocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interact(Transform target)
+    {
+        // This function is intended to be overriden
+        if (CanInteract(target))
+        {
+            Debug.Log("Interacted with " + target.name);
+
+        }
+    }
+
+    public override bool CanInteract(Transform target)
+    {
+        return Vector3.Distance(transform.position, target.transform.position) <= radius &&
+               (target.gameObject.GetComponent<Interactable>().colour == colour);
+    }
+}
