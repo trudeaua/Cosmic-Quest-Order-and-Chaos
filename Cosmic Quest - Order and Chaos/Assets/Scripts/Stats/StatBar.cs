6a7823a14e035a3779c9ccbfb5e319d32d5fdac2
Cam Van Ravens <camvanravens@outlook.com>
2019-11-25 00:34:29 -0500

    Added RenegerableStat on changed delegate event, health bars working

---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Stats/StatBar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatBar : MonoBehaviour
+{
+    public Image healthBar;
+    public Image manaBar;
+    
+    private RegenerableStat _healthStat;
+    private RegenerableStat _manaStat;
+
+    private void Awake()
+    {
+        PlayerStatsController stats = transform.root.GetComponent<PlayerStatsController>();
+        _healthStat = stats.health;
+        _manaStat = stats.mana;
+    }
+
+    private void Start()
+    {
+        _healthStat.onCurrentValueChanged += UpdateHealthValue;
+        _manaStat.onCurrentValueChanged += UpdateManaValue;
+    }
+
+    private void UpdateHealthValue(float value)
+    {
+        healthBar.fillAmount = value / _healthStat.maxValue;
+    }
+    
+    private void UpdateManaValue(float value)
+    {
+        manaBar.fillAmount = value / _manaStat.maxValue;
+    }
+}
