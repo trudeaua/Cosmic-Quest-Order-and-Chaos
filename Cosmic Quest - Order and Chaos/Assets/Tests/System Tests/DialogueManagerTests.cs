b23651a6d332af81d94c882e794117dc1348f641
Tommy Tran <trant28@mcmaster.ca>
2019-12-01 23:54:09 -0500

    DialogueManager, RegenerableStats tests

---
+++ Cosmic Quest - Order and Chaos/Assets/Tests/System Tests/DialogueManagerTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DialogueManagerTests
+{
+    DialogueManager dialogueManager;
+    Dialogue testDialogue;
+    Animator anim;
+
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        SetupTestDialogue();
+        yield return LoadScene();
+    }
+
+    private IEnumerator LoadScene()
+    {
+        // Load Test scene
+        AsyncOperation sceneLoading = SceneManager.LoadSceneAsync("TestScene", LoadSceneMode.Single);
+
+        float timer = 0;
+        while (!sceneLoading.isDone)
+        {
+            if (timer > 15f)
+            {
+                break;
+            }
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        Assert.IsTrue(sceneLoading.isDone, "Scene load timed out! The scene could not be loaded.");
+    }
+
+    private void SetupTestDialogue()
+    {
+        string name = "Random Character";
+        string sentence1 = "Test for sentence 1";
+        string sentence2 = "Test for sentence 2";
+        testDialogue = new Dialogue()
+        {
+            sentences = new string[] { sentence1, sentence2 },
+            name = name,
+        };
+    }
+
+    [UnityTest]
+    public IEnumerator DialogueManager_StartDialogue_ShouldModifyDialogueComponents()
+    {
+        GameObject go = MonoBehaviour.Instantiate(TestResourceManager.Instance.GetResource("Dialogue Manager"), Vector3.zero, Quaternion.identity);
+        dialogueManager = go.GetComponent<DialogueManager>();
+
+        TextMeshProUGUI name = dialogueManager.dialogueName;
+        TextMeshProUGUI text = dialogueManager.dialogueText;
+
+        string initialName = name.text;
+        string initialText = text.text;
+
+        dialogueManager.StartDialogue(testDialogue, false);
+        Assert.AreNotEqual(initialName, name.text);
+        Assert.AreNotEqual(initialText, text.text);
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator DialogueManager_StartDialogue_ShouldShowDialogueBox()
+    {
+        GameObject go = MonoBehaviour.Instantiate(TestResourceManager.Instance.GetResource("Dialogue Manager"), Vector3.zero, Quaternion.identity);
+        dialogueManager = go.GetComponent<DialogueManager>();
+        anim = dialogueManager.anim;
+        Assert.AreEqual(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name, "DialogueBoxClose");
+        dialogueManager.StartDialogue(testDialogue, false);
+        yield return new WaitForSeconds(0.3f);
+        Assert.AreEqual(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name, "DialogueBoxOpen");
+    }
+
+    [UnityTest]
+    public IEnumerator DialogueManager_ShouldCloseDialogueAfterSentencesFinish()
+    {
+        GameObject go = MonoBehaviour.Instantiate(TestResourceManager.Instance.GetResource("Dialogue Manager"), Vector3.zero, Quaternion.identity);
+        dialogueManager = go.GetComponent<DialogueManager>();
+        anim = dialogueManager.anim;
+        dialogueManager.StartDialogue(testDialogue, false);
+        yield return new WaitForSeconds(4f);
+        Assert.AreEqual(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name, "DialogueBoxClose");
+    }
+}
---
+++ Cosmic Quest - Order and Chaos/Assets/Tests/Unit Tests/RegenerableStatsTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class RegenerableStatTests
+    {
+        RegenerableStat instance;
+
+        [SetUp]
+        public void Setup()
+        {
+            instance = new RegenerableStat();
+            instance.maxValue = 10f;
+            instance.minValue = 0f;
+        }
+
+        [Test]
+        public void Init_SetsCurrentValueToMax()
+        {
+            Assert.Zero(instance.CurrentValue);
+            instance.Init();
+            Assert.AreEqual(instance.maxValue, instance.CurrentValue);
+        }
+
+        [Test]
+        public void Add_ShouldSetCurrentValueProperly()
+        {
+            float val1 = 5f;
+            float val2 = 4f;
+
+            instance.Add(val1);
+            Assert.AreEqual(val1, instance.CurrentValue);
+            instance.Add(val2);
+            Assert.AreEqual(val1 + val2, instance.CurrentValue);
+            instance.Add(instance.maxValue + 1f);
+            Assert.AreEqual(instance.maxValue, instance.CurrentValue);
+        }
+
+        [Test]
+        public void Subtract_ShouldSetCurrentValueProperly()
+        {
+            float val = instance.maxValue / 2;
+            instance.Init();
+
+            instance.Subtract(val);
+            Assert.AreEqual(instance.maxValue - val, instance.CurrentValue);
+
+            // Reached minimum value
+            instance.Subtract(val);
+            Assert.AreEqual(instance.minValue, instance.CurrentValue);
+
+            // Stays at minimum value
+            instance.Subtract(val);
+            Assert.AreEqual(instance.minValue, instance.CurrentValue);
+        }
+    }
+}
