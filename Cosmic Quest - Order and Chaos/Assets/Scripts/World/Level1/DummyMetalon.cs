934a0b148bc4b2ea7212177778d7b52220445576
nessa-art <nessenceart@gmail.com>
2019-12-19 01:44:31 -0500

    adding room 3 dummy metalons

---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/World/Level1/DummyMetalon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DummyMetalon : MonoBehaviour
+{
+    public Animator Anim;
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();   
+        _agent = GetComponent<NavMeshAgent>();
+        Anim.SetFloat("WalkSpeed", 5f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
