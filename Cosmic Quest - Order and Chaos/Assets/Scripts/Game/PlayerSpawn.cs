f8bade3f4ce6ea8e43346f4890c3af91f795f118
Cam Van Ravens <camvanravens@outlook.com>
2020-02-28 23:09:20 -0500

    mathematics

--- Cosmic Quest - Order and Chaos/Assets/Scripts/Game/PlayerSpawn.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Game/PlayerSpawn.cs
@@ -2,7 +2,7 @@
 
 public class PlayerSpawn : MonoBehaviour
 {
-    public float radius = 2.0f;
+    public float radius;
     
     private void Start()
     {
@@ -14,7 +14,7 @@ public class PlayerSpawn : MonoBehaviour
         // Instantiate and place player characters evenly around spawn point
         for (int i = 0; i < numPlayers; i++)
         {
-            Vector3 offset = new Vector3(Mathf.Sin((i*Mathf.PI) / numPlayers), 0f, Mathf.Cos((i*Mathf.PI) / numPlayers));
+            Vector3 offset = new Vector3(Mathf.Cos((2*i*Mathf.PI) / numPlayers), 0f, Mathf.Sin((2*i*Mathf.PI) / numPlayers));
             GameObject player = PlayerManager.Instance.InstantiatePlayer(i);
             player.transform.position = new Vector3(spawnPos.x, 0f, spawnPos.z) + radius * offset;
             PlayerManager.Instance.RegisterPlayer(player);
