192017b2a964f00cc32bc09a438a9c9390c48b49
trudeaua <Alex>
2020-03-20 11:28:37 -0400

    Skip press x screen when player goes to main menu from a level

--- Cosmic Quest - Order and Chaos/Assets/Scripts/Game/PlayerUIControlSpawn.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Game/PlayerUIControlSpawn.cs
@@ -1,15 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerUIControlSpawn : MonoBehaviour
 {
+    [Tooltip("Seconds to wait for game state to not be loading")]
+    public float timeout = 10;
     private void Start()
     {
-        int numPlayers = PlayerManager.Instance.NumPlayers;
-
-        // Instantiate and place player characters evenly around spawn point
-        for (int i = 0; i < numPlayers; i++)
+        StartCoroutine(Spawn());
+    }
+    private IEnumerator Spawn()
+    {
+        float elapsed = 0;
+        while (elapsed < timeout)
         {
-            GameObject playerUIControl = PlayerManager.Instance.InstantiatePlayerUIControl(i);
+            if (GameManager.Instance.CurrentState == GameManager.GameState.Loading)
+            {
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+            else
+            {
+                int numPlayers = PlayerManager.Instance.NumPlayers;
+
+                // Instantiate and place player characters evenly around spawn point
+                for (int i = 0; i < numPlayers; i++)
+                {
+                    GameObject playerUIControl = PlayerManager.Instance.InstantiatePlayerUIControl(i);
+                }
+                break;
+            }
         }
     }
 }
