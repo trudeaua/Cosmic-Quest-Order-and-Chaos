d3c677ee729e48a75272c2bc897875046cf4a8c5
Vanessa Truong <v.truong9710@outlook.com>
2020-04-09 20:37:04 -0400

    Adding dialogue trigger to boss battle initiation

--- Cosmic Quest - Order and Chaos/Assets/Scripts/World/Puzzles/PuzzleListener.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/World/Puzzles/PuzzleListener.cs
@@ -6,8 +6,14 @@ public class PuzzleListener : MonoBehaviour
 {
     [Tooltip("Puzzles needing completion for Boss battle")]
     public Puzzle[] Puzzles;
+    
+    [Tooltip("Dialogue to play after completing all puzzles")]
+    public DialogueTrigger dialogueTrigger;
+
     public GameObject bossDoor; 
     public int numCompleted;
+
+
 
     public void IncrementCompletedPuzzles()
     {
@@ -15,6 +21,7 @@ public class PuzzleListener : MonoBehaviour
         if (numCompleted == Puzzles.Length)
         {
             bossDoor.SetActive(true);
+            dialogueTrigger.TriggerDialogue();
         }
     }
 }
